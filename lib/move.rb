27ba8cbbeac8ba5ab38da4c60ac8b8164c5f481c
Julian Brown <julianxbrown@gmail.com>
2018-12-02 23:55:43 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,14 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+  input.to_i - 1 
+end
+
+
+def move(array, index, value = "x")
+  puts 
+end
+#return the modified array with the updated index corresponding to the player's token. Don't create a new local variable for the board array, modify the one passed in as the argument and return it.
+#Part of your #move method will mean updating the board Array passed into it.
